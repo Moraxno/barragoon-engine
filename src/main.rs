4ea1df2fcf8d999890afffc06aa804a1a2d98329
Moraxno <robertbock.98@googlemail.com>
2024-08-05 22:08:41 +0200

    TASK: Added formatting.

--- src/main.rs
+++ src/main.rs
@@ -1,67 +1,69 @@
-#[derive(Debug,Copy,Clone)]
+#[derive(Debug, Copy, Clone)]
 enum TileType {
     Two,
     Three,
-    Four
+    Four,
 }
-#[derive(Debug,Copy,Clone)]
+#[derive(Debug, Copy, Clone)]
 enum Player {
     White,
-    Brown
+    Brown,
 }
-#[derive(Debug,Copy,Clone)]
+#[derive(Debug, Copy, Clone)]
 enum BarragoonOrientation {
     North,
     West,
     South,
-    East
+    East,
 }
-#[derive(Debug,Copy,Clone)]
+#[derive(Debug, Copy, Clone)]
 enum BarragoonAlignment {
     Horizontal,
-    Vertical
+    Vertical,
 }
-#[derive(Debug,Copy,Clone)]
+#[derive(Debug, Copy, Clone)]
 enum BarragoonFace {
     Blocking,
-    Straight {alignment: BarragoonAlignment},
-    OneWay {orientation: BarragoonOrientation},
-    OneWayTurnLeft {orientation: BarragoonOrientation},
-    OneWayTurnRight {orientation: BarragoonOrientation},
-    ForceTurn
+    Straight { alignment: BarragoonAlignment },
+    OneWay { orientation: BarragoonOrientation },
+    OneWayTurnLeft { orientation: BarragoonOrientation },
+    OneWayTurnRight { orientation: BarragoonOrientation },
+    ForceTurn,
 }
 
-#[derive(Debug,Copy,Clone)]
+#[derive(Debug, Copy, Clone)]
 enum SquareContent {
     Empty,
     Tile(TileType, Player),
-    Barragoon(BarragoonFace)
+    Barragoon(BarragoonFace),
 }
-
 
 type SC = SquareContent;
 
-#[derive(Debug,Copy,Clone)]
+#[derive(Debug, Copy, Clone)]
 struct Game {
-    board: [[SC; 7]; 9]
+    board: [[SC; 7]; 9],
 }
 
-#[derive(Debug,Copy,Clone)]
+#[derive(Debug, Copy, Clone)]
 enum FenError {
-    UnderfullLine {char_index: usize},
-    OverfullLine{char_index: usize},
-    TooManyLines{char_index: usize},
-    InvalidChar{char_index: usize},
+    UnderfullLine { char_index: usize },
+    OverfullLine { char_index: usize },
+    TooManyLines { char_index: usize },
+    InvalidChar { char_index: usize },
 }
 
-#[derive(Debug,Copy,Clone)]
+#[derive(Debug, Copy, Clone)]
 enum FenParseObject {
     JumpCol(u32),
     SkipRow,
     Square(SquareContent),
-    InvalidChar
+    InvalidChar,
 }
-
+type FPO = FenParseObject;
+type BA = BarragoonAlignment;
+type BO = BarragoonOrientation;
+type BF = BarragoonFace;
 
 impl Game {
     pub fn new(fen: &str) -> Result<Game, FenError> {
@@ -72,67 +74,63 @@ impl Game {
 
         for (index, c) in fen.char_indices() {
             let obj: FenParseObject = match c {
-                'Z'       => FenParseObject::Square(SC::Tile(TileType::Two, Player::White)),
-                'z'       => FenParseObject::Square(SC::Tile(TileType::Two, Player::Brown)),
-                'D'       => FenParseObject::Square(SC::Tile(TileType::Three, Player::White)),
-                'd'       => FenParseObject::Square(SC::Tile(TileType::Three, Player::Brown)),
-                'V'       => FenParseObject::Square(SC::Tile(TileType::Four, Player::White)),
-                'v'       => FenParseObject::Square(SC::Tile(TileType::Four, Player::Brown)),
-                '1'..='7' => FenParseObject::JumpCol(c.to_digit(10).unwrap()),
-                '/'       => FenParseObject::SkipRow,
-                '+'       => FenParseObject::Square(SC::Barragoon(BarragoonFace::ForceTurn)),
-                '|'       => FenParseObject::Square(SC::Barragoon(BarragoonFace::Straight { alignment: BarragoonAlignment::Vertical })),
-                '-'       => FenParseObject::Square(SC::Barragoon(BarragoonFace::Straight { alignment: BarragoonAlignment::Horizontal })),
-                'Y'       => FenParseObject::Square(SC::Barragoon(BarragoonFace::OneWay { orientation: BarragoonOrientation::South })),
-                '^'       => FenParseObject::Square(SC::Barragoon(BarragoonFace::OneWay { orientation: BarragoonOrientation::North })),
-                '<'       => FenParseObject::Square(SC::Barragoon(BarragoonFace::OneWay { orientation: BarragoonOrientation::West })),
-                '>'       => FenParseObject::Square(SC::Barragoon(BarragoonFace::OneWay { orientation: BarragoonOrientation::East })),
-                'x'       => FenParseObject::Square(SC::Barragoon(BarragoonFace::Blocking)),
-                'S'       => FenParseObject::Square(SC::Barragoon(BarragoonFace::OneWayTurnLeft { orientation: BarragoonOrientation::South })),
-                'N'       => FenParseObject::Square(SC::Barragoon(BarragoonFace::OneWayTurnLeft { orientation: BarragoonOrientation::North })),
-                'E'       => FenParseObject::Square(SC::Barragoon(BarragoonFace::OneWayTurnLeft { orientation: BarragoonOrientation::East })),
-                'W'       => FenParseObject::Square(SC::Barragoon(BarragoonFace::OneWayTurnLeft { orientation: BarragoonOrientation::West })),
-                's'       => FenParseObject::Square(SC::Barragoon(BarragoonFace::OneWayTurnRight { orientation: BarragoonOrientation::South })),
-                'n'       => FenParseObject::Square(SC::Barragoon(BarragoonFace::OneWayTurnRight { orientation: BarragoonOrientation::North })),
-                'e'       => FenParseObject::Square(SC::Barragoon(BarragoonFace::OneWayTurnRight { orientation: BarragoonOrientation::East })),
-                'w'       => FenParseObject::Square(SC::Barragoon(BarragoonFace::OneWayTurnRight { orientation: BarragoonOrientation::West })),
-                _ => FenParseObject::InvalidChar
+                'Z' => FPO::Square(SC::Tile(TileType::Two, Player::White)),
+                'z' => FPO::Square(SC::Tile(TileType::Two, Player::Brown)),
+                'D' => FPO::Square(SC::Tile(TileType::Three, Player::White)),
+                'd' => FPO::Square(SC::Tile(TileType::Three, Player::Brown)),
+                'V' => FPO::Square(SC::Tile(TileType::Four, Player::White)),
+                'v' => FPO::Square(SC::Tile(TileType::Four, Player::Brown)),
+                '+' => FPO::Square(SC::Barragoon(BF::ForceTurn)),
+                '|' => FPO::Square(SC::Barragoon(BF::Straight { alignment: BA::Vertical })),
+                '-' => FPO::Square(SC::Barragoon(BF::Straight { alignment: BA::Horizontal })),
+                'Y' => FPO::Square(SC::Barragoon(BF::OneWay { orientation: BO::South })),
+                '^' => FPO::Square(SC::Barragoon(BF::OneWay { orientation: BO::North })),
+                '<' => FPO::Square(SC::Barragoon(BF::OneWay { orientation: BO::West })),
+                '>' => FPO::Square(SC::Barragoon(BF::OneWay { orientation: BO::East })),
+                'x' => FPO::Square(SC::Barragoon(BF::Blocking)),
+                'S' => FPO::Square(SC::Barragoon(BF::OneWayTurnLeft { orientation: BO::South })),
+                'N' => FPO::Square(SC::Barragoon(BF::OneWayTurnLeft { orientation: BO::North })),
+                'E' => FPO::Square(SC::Barragoon(BF::OneWayTurnLeft { orientation: BO::East })),
+                'W' => FPO::Square(SC::Barragoon(BF::OneWayTurnLeft { orientation: BO::West })),
+                's' => FPO::Square(SC::Barragoon(BF::OneWayTurnRight { orientation: BO::South })),
+                'n' => FPO::Square(SC::Barragoon(BF::OneWayTurnRight { orientation: BO::North })),
+                'e' => FPO::Square(SC::Barragoon(BF::OneWayTurnRight { orientation: BO::East })),
+                'w' => FPO::Square(SC::Barragoon(BF::OneWayTurnRight { orientation: BO::West })),
+                '1'..='7' => FPO::JumpCol(c.to_digit(10).unwrap()),
+                '/' => FPO::SkipRow,
+                _ => FPO::InvalidChar,
             };
 
-            match obj { 
-                FenParseObject::Square(content) => {
+            match obj {
+                FPO::Square(content) => {
                     board[row_ptr as usize][col_ptr as usize] = content;
                     col_ptr += 1;
-                },
-                FenParseObject::JumpCol(cols) => {
+                }
+                FPO::JumpCol(cols) => {
                     col_ptr += cols;
-                    if col_ptr > 7 { 
-                        return Result::Err(FenError::OverfullLine{char_index: index}); 
+                    if col_ptr > 7 {
+                        return Result::Err(FenError::OverfullLine { char_index: index });
                     }
-                },
-                FenParseObject::SkipRow => {
+                }
+                FPO::SkipRow => {
                     if col_ptr == 7 {
                         col_ptr = 0;
                         row_ptr += 1;
                     }
 
                     if row_ptr > 8 {
-                        return Result::Err(FenError::TooManyLines{char_index: index}); 
+                        return Result::Err(FenError::TooManyLines { char_index: index });
                     }
-                },
-                FenParseObject::InvalidChar => {
-                    return Result::Err(FenError::InvalidChar {char_index: index}); 
+                }
+                FPO::InvalidChar => {
+                    return Result::Err(FenError::InvalidChar { char_index: index });
                 }
             }
         }
 
-
-        return Ok(Game {
-            board: board
-        })
+        return Ok(Game { board: board });
     }
 }
-
 
 fn main() {
     println!("Hello, world!");
